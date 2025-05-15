f651912cffc49571961329b895aaa59e6b07e305
Diego <iwujigoodluck5@gmail.com>
2025-05-15 21:41:35 +0000

    Update HomeScreen.jsx

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,20 +1,19 @@
-import HeroSection from "../components/homePage-Components/heroSection";
+import HeroSection from "../components/homePage-Components/HeroSection";
 import SectionTwo from "../components/homePage-Components/SectionTwo";
 import SectionThree from "../components/homePage-Components/SectionThree";
 import OurProducts from "../components/homePage-Components/OurProducts";
 import CreativeCrew from "../components/homePage-Components/creativeCrew";
 import ReadySection from "../components/homePage-Components/readySection";
 
-
 function HomeScreen() {
   return (
     <main>
-        <HeroSection />
-        <SectionTwo />
-        <SectionThree />
-        <OurProducts />
-        <CreativeCrew />
-        <ReadySection />
+      <HeroSection />
+      <SectionTwo />
+      <SectionThree />
+      <OurProducts />
+      <CreativeCrew />
+      <ReadySection />
     </main>
   );
 }
