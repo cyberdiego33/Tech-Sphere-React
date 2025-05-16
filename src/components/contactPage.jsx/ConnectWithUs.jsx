96a80e936b656f56382bd1c4524fa1170dc24dd6
Diego <iwujigoodluck5@gmail.com>
2025-05-16 17:23:22 +0000

    Update ConnectWithUs.jsx

--- src/components/contactPage.jsx/ConnectWithUs.jsx
+++ src/components/contactPage.jsx/ConnectWithUs.jsx
@@ -6,21 +6,18 @@ const SocialsApi = [
     socialMedia: "Email Us At",
     socialHandle: "techsphere@gmail.com",
     socialIcon: shield,
-    socialName: 'shield'
   },
   {
     index: 2,
     socialMedia: "Connect With Us At",
     socialHandle: "techsphere@LinkedIn.com",
     socialIcon: shield,
-    socialName: 'shield'
   },
   {
     index: 3,
     socialMedia: "Follow Us At",
     socialHandle: "techsphere@Twitter.com",
     socialIcon: shield,
-    socialName: 'shield'
   },
 ];
 
@@ -65,13 +62,57 @@ const ConnectWithUs = function () {
             </p>
 
             {SocialsApi.map(function (items) {
-              return <Socials socialMedia={items.socialMedia} socialIcon={items.socialIcon} socialHandle={items.socialHandle} index={items.index} socialName={items.socialName}/>;
+              return (
+                <Socials
+                  socialMedia={items.socialMedia}
+                  socialIcon={items.socialIcon}
+                  socialHandle={items.socialHandle}
+                  index={items.index}
+                  socialName={items.socialName}
+                />
+              );
             })}
           </div>
 
           <div className="p-8 bg-gradient-to-b from-gray-800 to-gray-900 rounded-xl shadow-sm border border-gray-700">
             <div className="mb-6 size-7 bg-gradient-to-r from-blue-600 to-purple-600 rounded-full">
               <img src={shield} alt="chat logo" />
+            </div>
+
+            <h2 className="text-2xl font-bold text-white mb-6">
+              Send Us a message
+            </h2>
+            <p className="text-gray-400 mb-8">
+              Have questions about our services? You can Email Us Here. Fill in
+              the form
+            </p>
+            <div>
+              <form action="">
+                <div className="flex items-center p-4 bg-gray-800/50 rounded-lg border border-gray-700 hover:bg-gray-800 hover:border-blue-500 group transition-all mb-4">
+                  <input
+                    type="text"
+                    placeholder="FullName"
+                    className="w-full text-gray-400 outline-0"
+                    required
+                  />
+                </div>
+                <div className="flex items-center p-4 bg-gray-800/50 rounded-lg border border-gray-700 hover:bg-gray-800 hover:border-blue-500 group transition-all mb-4">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    className="w-full text-gray-400 outline-0"
+                    required
+                  />
+                </div>
+                <div className="flex items-center p-4 bg-gray-800/50 rounded-lg border border-gray-700 hover:bg-gray-800 hover:border-blue-500 group transition-all mb-4 ">
+                  <input
+                    type="textarea"
+                    placeholder="Type your message"
+                    className="w-full text-gray-400 outline-0 text-wrap"
+                    required
+                  />
+                </div>
+              </form>
             </div>
           </div>
         </div>
